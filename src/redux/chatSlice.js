8064c9ab484a9285499cb83376683c71dbb4e732
ZubairAmin695 <zubairamin695@gmail.com>
2023-09-21 13:01:10 +0500

    chat slice added

---
+++ src/redux/chatSlice.js
@@ -0,0 +1,17 @@
+// src/redux/chatSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const chatSlice = createSlice({
+  name: 'chat',
+  initialState: {
+    messages: [],
+  },
+  reducers: {
+    addMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
+  },
+});
+
+export const { addMessage } = chatSlice.actions;
+export default chatSlice.reducer;
